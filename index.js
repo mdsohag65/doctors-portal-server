7001cc68adc9e0e245c325954bd44b95254ad70a
mdsohag65 <sohag92ahmed@gmail.com>
2022-05-25 11:37:09 +0600

    create a new booking api for nested route

--- index.js
+++ index.js
@@ -55,6 +55,13 @@ async function run() {
          * app.delete('/booking/:id') // delete specific one
          */
 
+        app.get('/booking', async (req, res) => {
+            const patient = req.query.patient;
+            const query = { patient: patient };
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+        });
+
         app.post('/booking', async (req, res) => {
             const booking = req.body;
             const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
